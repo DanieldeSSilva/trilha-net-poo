4a3cf5d407532bbf3d57589b1b520af9398925fa
DanieldeSSilva <danieldes.silva@gmail.com>
2023-10-27 01:56:56 -0300

    Conclusão do .net programação orientada a objeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone novoIphone = new("99999999", "Galaxi", "123123", 123);
+Nokia novoNokia = new("66666666", "Nokia exemplo", "987987987", 987);
+
+novoIphone.Ligar();
+novoNokia.Ligar();
+
+
+novoIphone.ReceberLigacao();
+novoNokia.ReceberLigacao();
+
+novoIphone.InstalarAplicativo("Whatsup");
+novoNokia.InstalarAplicativo("YouTub");
